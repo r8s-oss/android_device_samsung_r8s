67371208818aed828f0be26305e21f5705fb3116
Roshan Pius <rpius@google.com>
2020-04-24 14:33:28 -0700

    r8s: Add WifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: Iec27a6d947a232a42451ef095dc73d1a52327db5

--- device.mk
+++ device.mk
@@ -285,7 +285,8 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
     wpa_supplicant \
-    wpa_supplicant.conf
+    wpa_supplicant.conf \
+    WifiOverlay
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
