14f6aad14db283d773b18e08cc6ae46347b6ba4e
doomvrr <doomurr@gmail.com>
2022-08-19 13:15:06 +0000

    r8s: add dalvik config
    Change-Id: Ic14e68bfbb9d08924a5579d0c31c48c6a11a650a

--- device.mk
+++ device.mk
@@ -13,6 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 ## Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+## Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/samsung/r8s/r8s-vendor.mk)
 
